31e9beb6323c1eb07c7923ecb27f79da5869e0b9
STojal <simaotojal@ua.pt>
2023-12-22 14:11:07 +0000

    ja esta a dar para entrar e sair

--- entrar.js
+++ entrar.js
@@ -7,13 +7,18 @@ function utilizador(){
     if($("#username").val() == "Teste" && $("#password").val() == "1234abcd"){
         console.log(" entraste");
         window.location.href = "MyProfile.html";
+        localStorage.setItem("entrada", 1)
     }
     else if($("#username").val() === "Vet" && $("#password").val() === "1234abcd"){
         window.location.href = "MyProfileVet.html";
+        localStorage.setItem("entrada", 2)
+
     }
     
     else{
         alert("Não entraste");
+        localStorage.setItem("entrada", 0)
+
     }
     
     }
