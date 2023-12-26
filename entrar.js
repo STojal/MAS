f65646929b3338f49207ae395997a2072a55d851
STojal <simaotojal@ua.pt>
2023-12-26 10:37:22 +0000

    a

---
+++ entrar.js
@@ -0,0 +1,24 @@
+
+
+function utilizador(){
+
+
+    console.log($("#username").val())
+    if($("#username").val() == "Teste" && $("#password").val() == "1234abcd"){
+        console.log(" entraste");
+        window.location.href = "MyProfile.html";
+        localStorage.setItem("entrada", 1)
+    }
+    else if($("#username").val() === "Vet" && $("#password").val() === "1234abcd"){
+        window.location.href = "MyProfileVet.html";
+        localStorage.setItem("entrada", 2)
+
+    }
+    
+    else{
+        alert("Não entraste");
+        localStorage.setItem("entrada", 0)
+
+    }
+    
+    }
